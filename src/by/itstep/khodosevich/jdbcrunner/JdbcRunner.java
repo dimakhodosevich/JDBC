b39e8c7ea410434d2bf4b0ef7ab43459c2f9ab55
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-10-30 13:35:36 +0300

    Using executeQuery() and next() to using output(input) our date. Don't advice to use update in this way.

--- src/by/itstep/khodosevich/jdbcrunner/JdbcRunner.java
+++ src/by/itstep/khodosevich/jdbcrunner/JdbcRunner.java
@@ -3,6 +3,8 @@ package by.itstep.khodosevich.jdbcrunner;
 import by.itstep.khodosevich.jdbcrunner.util.ConnectionManager;
 import org.postgresql.Driver;
 
+import java.math.BigDecimal;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,22 +14,30 @@ public class JdbcRunner {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                    UPDATE info 
-                    SET data = 'TestTest'
-                    WHERE id = 5
-                    RETURNING *
-                    ;
+                    SELECT * 
+                    FROM ticket;
                 """;
 
         try (var connection = ConnectionManager.open();
+//             using for manipulate our results(change it)
+//             var statement =
+//                     connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+
              var statement = connection.createStatement();
         ) {
-            var executeResult = statement.execute(sql);
+            var executeResult = statement.executeQuery(sql);
             System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
             System.out.println(executeResult);
-            System.out.println(statement.getUpdateCount());
+            while (executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+//                when add arguments to our connection statement below
+//                executeResult.updateBigDecimal("cost", BigDecimal.valueOf(1.22222));
 
+                System.out.println("------------");
+            }
         }
     }
 }

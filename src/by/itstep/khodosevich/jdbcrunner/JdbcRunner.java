461732c984ee99a916237073ad62a47f484b9149
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-10-30 12:59:04 +0300

    First look how to use Statement.class for using DDL(data definition language) at Intellij Idea. We use statement.execute("our sql command") to manipulate and create tables, schemas, database. Don't forget always update tables into setting database for pull data.

--- src/by/itstep/khodosevich/jdbcrunner/JdbcRunner.java
+++ src/by/itstep/khodosevich/jdbcrunner/JdbcRunner.java
@@ -4,16 +4,26 @@ import by.itstep.khodosevich.jdbcrunner.util.ConnectionManager;
 import org.postgresql.Driver;
 
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 public class JdbcRunner {
 
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
+        String sql = """
+                    CREATE TABLE IF NOT EXISTS info(
+                    id SERIAL PRIMARY KEY ,
+                    data TEXT NOT NULL
+                    );
+                """;
 
         try (var connection = ConnectionManager.open();
+             var statement = connection.createStatement();
         ) {
             System.out.println(connection.getTransactionIsolation());
+            var executeResult = statement.execute(sql);
+            System.out.println(executeResult);
         }
     }
 }
